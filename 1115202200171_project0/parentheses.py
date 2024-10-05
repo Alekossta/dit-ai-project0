b837d76c17776026153b9c79a97498c404bd358d
Alekossta <alekosstavrinoudakis@gmail.com>
2024-10-05 17:21:16 +0300

    fixed q4 based on piazza answer and expected value

--- 1115202200171_project0/parentheses.py
+++ 1115202200171_project0/parentheses.py
@@ -1,23 +1,31 @@
 # We assume our lists will not have any character that is a space
 # We assume we want our expressions to have parentheses with two numbers inside
 
+allowedOperators = ["*", "+", "-", "^", "/"]
+
 def complete_parentheses(numberExpressionList):
     newList = numberExpressionList.copy()
     parenthesesNum = 0
-    sequenceCounter = 0
+    operatorCounter = 0
+    operandCounter = 0
     # we start the iteration from the end
     for i in range(len(newList) - 1, -1, -1):
         element = newList[i]
-        if(element == ")"):
-            parenthesesNum += 1
-            sequenceCounter = 0
-        else:
-            sequenceCounter +=1
+        if(element in allowedOperators):
+            operatorCounter+=1
+        elif(element == ")"):
+            parenthesesNum+=1
+        else: #will be a number
+            operandCounter+=1
 
-        if(sequenceCounter == 3 and parenthesesNum > 0):
-            newList.insert(i,"(")
+        if(operandCounter == 2 and operatorCounter >= 1):
+            newList.insert(i, "(")
+            operandCounter-=2
             parenthesesNum-=1
-            sequenceCounter=1 # set it to 1 because the new complete parentheses counts as new "sequence"
+            operatorCounter-=1
+            if(operatorCounter > parenthesesNum - 2):
+                newList.insert(i,"(")
+                parenthesesNum-=1
 
 
     for i in range(1,parenthesesNum+1):
@@ -36,11 +44,7 @@ def print_expression(expression):
 exampleExpression1 = ["1","+","2",")","*","3","-","4",")","*","5","-","6",")",")",")"]
 exampleExpression2 = [")"]
 exampleExpression3 = ["0",")"]
-exampleExpression4 = ["1", "+", "2", "+", "3", ")"]
-hardExpression = ["1", "*", "1", "+", "2", "+", "3", ")", ")"]
 
 print_expression(exampleExpression1)
 print_expression(exampleExpression2)
 print_expression(exampleExpression3)
-print_expression(exampleExpression4)
-print_expression(hardExpression)
